02339c7cd73153316433ae7699630c48465e6316
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-12 13:57:26 -0300

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -13,17 +13,19 @@ export default function Form() {
   <form onSubmit={handleSubmit}> 
     <label> 
      To:{' '} 
-     <select 
-       value ={to}
-       onChange= {e => setTo(e.target.value)}> 
-       <option value="Alice">Alice</option> <option value="Bob">Bob</option>
-</select>
-</label>
-<textarea
-placeholder="Message"
-value=[message)
-onChange=fe => setMessage(e.target.value)) |7
-<button type="submit">Send</button>
-</form>
+      <select 
+         value ={to}
+         onChange= {e => setTo(e.target.value)}> 
+         <option value="Alice">Alice</option> 
+         <option value="Bob">Bob</option>
+      </select>
+   </label>
+   <textarea
+      placeholder="Message"
+      value={message}
+      onChange={e => setMessage(e.target.value)}
+    />
+   <button type="submit">Send</button>
+  </form>
 );
 }
