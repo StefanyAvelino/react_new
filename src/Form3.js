e3cbbf0fe8759abed20d13967ed9963abf2c2380
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-12 17:50:25 -0300

    Update Form3.js

--- src/Form3.js
+++ src/Form3.js
@@ -2,7 +2,36 @@ import { useState } from 'react';
 
 export default function Form3() { 
   const [person, setPerson] = useState ({ 
-    name: 'Niki de Saint Phalle', artwork: { title: 'Blue Nana', city: 'Hamburg', image: 'https://i.imgur.com/Sd1AgUOm.jpg', } }); function handleNameChange(e) ( setPerson (f ...person, name: e.target.value )); 3
-function handleTitleChange(e) ( setPerson ( ....person, artwork: ...person.artwork, title: e.target.value
-function handleCityChange(e) ( setPerson (f ...person, artwork: ..person.artwork, city: e.target.value ) 1); )
-function handleImageChange(e) ( setPerson (f ...person, artwork: S ..person.artwork, image: e.target.value t D);
+    name: 'Niki de Saint Phalle', 
+    artwork: { 
+      title: 'Blue Nana', 
+      city: 'Hamburg', 
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg', 
+    } 
+  }); 
+  function handleNameChange(e) { 
+    setPerson ({ 
+      ...person, 
+      name: e.target.value }); 
+  }
+  function handleTitleChange(e) { 
+    setPerson ({
+      ....person, 
+      artwork: {
+        ...person.artwork, 
+        title: e.target.value }});
+}
+function handleCityChange(e) { 
+  setPerson ({ 
+    ...person, 
+    artwork: {
+      ...person.artwork, 
+      city: e.target.value } });
+}
+function handleImageChange(e) { 
+  setPerson ({ 
+    ...person, 
+    artwork: { 
+      ...person.artwork, 
+      image: e.target.value }});
+}
