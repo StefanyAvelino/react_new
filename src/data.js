ef2098bbd1367699b39d4168130da67e8a094408
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-12 13:26:02 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -81,15 +81,23 @@ export const sculpturelist = [ {
   name: 'Terracotta Army',
   artist:'Unknown Artist',
   description: 'The Terracotta Army is a collection of terracotta sculptures depicting the armies of Qin Shi Huang, the first Emperor of China. The army consisted of more than 8,000 soldiers, 130 chariots with 520 horses, and 150 cavalry horses.',
-  url: 'https://i.ingur.com/HMFmH6m.jpg', alt: '12 terracotta sculptures of solemn warriors, each with a unique facial xpression and armor.
-name: 'Lunar Landscape', artist: 'Louise Nevelson',
-iescription: 'Nevelson was known for scavenging objects from New York City debris, which she would later assemble into monumental constructions. In this one, she usec disparate parts like bedpost, juggling pin, and seat fragment, nailing and gluing e them into boxes that reflect the influence O f Cubism's geometric abstraction of space and form.', url: 'https://i.imgur.com/xN7hY6om.jpg',
-alt: 'A black matte sculpture where the individual elements are initially indistinguishable.'
-name: 'Aureole',
-artist: 'Ranjani Shettar',
-description: 'Shettar merges the traditional and the modern, the natural and the industrial. Her art focuses on the relationship between man and nature. Her work was iescribed as compelling both abstractly and figuratively, gravity defying, and a "fine synthesis of unlikely materials."',
-'https://i.imqur.com/okTpbHhm.jpg', alt: 'A pale wire-like sculpture mounted on concrete wal7 and descending on the floor. It appears light.'
-name: 'Hippos',
-artist: 'Taipei Zoo',
-description: 'The raipei Zoo commissioned a Hippo Square featuring submerged hippos at plav.'. url: 'https://i.imgur.com/6o5Vuyu.jpg',
-alt: 'A group of bronze hippo sculptures emerging from the sett sidewalk as if they were simming.' H:
+  url: 'https://i.ingur.com/HMFmH6m.jpg',
+  alt: '12 terracotta sculptures of solemn warriors, each with a unique facial xpression and armor.
+},{
+  name: 'Lunar Landscape', artist: 'Louise Nevelson',
+  description: 'Nevelson was known for scavenging objects from New York City debris, which she would later assemble into monumental constructions. In this one, she usec disparate parts like bedpost, juggling pin, and seat fragment, nailing and gluing e them into boxes that reflect the influence O f Cubism s geometric abstraction of space and form.', 
+  url: 'https://i.imgur.com/xN7hY6om.jpg',
+  alt: 'A black matte sculpture where the individual elements are initially indistinguishable.'
+},{
+  name: 'Aureole',
+  artist: 'Ranjani Shettar',
+  description: 'Shettar merges the traditional and the modern, the natural and the industrial. Her art focuses on the relationship between man and nature. Her work was iescribed as compelling both abstractly and figuratively, gravity defying, and a "fine synthesis of unlikely materials."',
+  url: 'https://i.imqur.com/okTpbHhm.jpg',
+  alt: 'A pale wire-like sculpture mounted on concrete wal7 and descending on the floor. It appears light.'
+},{
+  name: 'Hippos',
+  artist: 'Taipei Zoo',
+  description: 'The raipei Zoo commissioned a Hippo Square featuring submerged hippos at plav.', 
+  url: 'https://i.imgur.com/6o5Vuyu.jpg',
+  alt: 'A group of bronze hippo sculptures emerging from the sett sidewalk as if they were simming.' 
+}];
