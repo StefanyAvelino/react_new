683dc60e4cdf6dd0180a06774c55bbda908fdce9
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-12 11:33:25 -0300

    Add files via upload

---
+++ src/Atv04.js
@@ -0,0 +1,17 @@
+import Toolbar from "./Toolbar";
+
+import { Link } from "react-router-dom";
+
+export default function Atv04(){
+    return(
+        <>
+            <h1>Atividades da Semana 4</h1>
+            <Toolbar/>
+            
+            
+            <Link to="/">Voltar</Link>
+        
+        </>
+    );
+
+}
