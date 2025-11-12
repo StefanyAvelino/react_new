e40804d5ff7df3c7009a42b7adf8446f959446f8
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-12 12:41:28 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -28,3 +28,35 @@ export const people = [{
   profession: 'astrophysicist',
   accomplishment: 'white dwarf star mass calculations', 
   imageId: 'lrWQx8l'}];
+
+export const sculpturelist = [ {
+  name: 'Homenaje la Neurocirugia',
+  artist: 'Marta Colvin Andrade',
+  description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most ecoanizshı. public art pieces.', 
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding human brain in their fingertips.'
+},{
+  name: 'Floralis Genérica', 
+  artist: 'Eduardo Catalano',
+  description: 'This enormous (75 ft. or 23m) siiver flower is located in Buenos Aires. It is designed to move, closing its petals in the evening or when strong winds blow and opening them in the mornina.', 
+  url: 'https://i.imgur.com/ZF6s192m.jpg',
+  alt: 'A gigantic metallid flower sculpture with reflective mirror-like petale and strong stamens.
+},{
+  name: 'Eternal Presence',
+  artist: 'John Woodrow Wilson', description: 'Wilson was known for his preoccupation with equality, social justice, as well as the essential and spiritual qualities of humankind. This massive (7ft. or 2,13m) bronze represents what he described as S symbolic Black presence infused with sense of universal humanity.', url:'https://i.imgur.com/artvpES.jpg", alt: 'The sculpture depicting human head seems ever-present and solemn. It radiates calm and serenity.'},{ name: 'Moai', artist: 'Unknown Artist', description: 'Located on the Easter Island, there are 1,000 moai, or extant mOnumen+a1 statues, created by the early Rapa Nui people, which some believe represented deified ancestors.', com/RCwLEoQm.jpg',
+url: 'https://i.imgur alt: 'Three monumental stone busts with the heads that are disproportionately large with somber faces.
+  },{name: 'Blue Nana', artist: 'Niki de Saint Phalle',
+Saint Phalle used creatures, symbols of femininity and description: 'The Nanas are triumphant fabric and found objects for the Nanas, maternity. Initially, and later on introduced polyester to achieve more vibrant effect.', url: 'https://i.imgur.com/Sd1AgUOm.jpg',
+alt: large mosaic sculpture of a whimsical dancing female figure in a colorfu? costume emanating joy.'
+  },{name: "Ultimate Form', artist: 'Barbara Hepworth',
+description: 'rhis abstract bronze sculpture is a part of The Family of Man series located at Yorkshire Sculpture Park. Hepworth chose Hot to -reate literal representations of the .arld but developed abstract forms inspired by people and landscapes.',
+url: 'https://i.imgur.com/2heNQDcm.jpg', alt: 'A tall sculpture made of three elements stacked on each other reminding of a human figure.' ), t
+},{name . 'Cavaliere',
+artist: 'Lamidi Olonade Fakeye', description: "Descended from four generations of woodcarvers, Fakeye's work blended raditiona1 and contemporary Yoruba hemes.". url: 'https://i.imgur.com/wIdGuZwm.png',
+alt: 'An intricate wood sculpture of Narrior with focused face on a horse adorned with patterns.'
+  },{
+name: 'Big Bellies',
+artist:
+'Alina Szapocznikow',
+description: "Szapocznikow is known for her sculptures of the fragmented body as a metaphor for the fragility and impermanence of youth and beauty. This sculpture Jepicts two very realistic large bellies stacked on top of each other, each around five feet (1,5m) tall.", url: 'https://i.imgur.com/AlHTAdDm.jpg',
+alt: 'The sculpture reminds cascade of folds, quite different from bellies in classical sculptures.'
