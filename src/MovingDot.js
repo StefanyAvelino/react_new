de9933c265907e14739561ff071ab0e9247828b9
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-13 08:49:12 -0300

    Update MovingDot.js

--- src/MovingDot.js
+++ src/MovingDot.js
@@ -1,9 +1,31 @@
-import ( useState ) from 'react'; export default function MovingDot() ( const [position, setPosition] = useState(f x: O, y: 0
-return (
-<div
-onPointerMove=e => position.x = e.clientX; position.y = e.clientY;
-style=[f
-position: 'relative', width: '100vw', height: '100vh',
-<div style= (
-position: 'absolute', backgroundColor: 'red', borderRadius: '50%', transform: `translate($fposition.x)px, $fposition.y]px)`, left: -10, top: -10, width: 20, height: 20,
-</div>
+import { useState } from 'react'; 
+
+export default function MovingDot() { 
+  const [position, setPosition] = useState({ 
+    x: 0, 
+    y: 0
+  });
+  return (
+    <div
+      onPointerMove= { e => { 
+        position.x = e.clientX;
+        position.y = e.clientY;
+    }}
+      style={{
+        position: 'relative', 
+        width: '100vw', 
+        height: '100vh',
+    }}>
+    <div style= {{
+      position: 'absolute', 
+      backgroundColor: 'red', 
+      borderRadius: '50%', 
+      transform: `translate(${position.x}px, ${position.y}px)`, 
+      left: -10, 
+      top: -10, 
+      width: 20, 
+      height: 20,
+      }} />
+    </div>
+  );
+}
