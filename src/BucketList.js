fd77bba3732b757c72ad7b2657c4e4212fcc7712
Stéfany Yasmin Avelino Passianoto <91292261+StefanyAvelino@users.noreply.github.com>
2025-11-16 20:40:22 -0300

    Update BucketList.js

--- src/BucketList.js
+++ src/BucketList.js
@@ -26,16 +26,41 @@ export default function BucketList() {
      );
     artwork.seen = nextSeen;
     setYourList(yourNextList);
-   }return (
-<
-<h1>Art Bucket List</hl>
-<h2>My list of art to see:</h2>
-<ItemList
-artworks=(myList) onToggle=/handleToggleMyList] />
-<h2>Your list of art to see:</h2>
-<ItemList
-artworks=[yourList)
-onToggle=(handleToggleYourList] />
-</>
-function ItemList(f artworks, onToggle )) ( return ( <ul> fartworks.map(artwork => ( <li key=fartwork.id)> <label> <input type="checkbox" checked=(artwork.seen) onChange=fe => ( onToggle( artwork.id, e.target.checked ): V (artwork.title)
-</label> </li> ))H </ul> ):
+   }
+   return (
+     <>
+       <h1>Art Bucket List</h1>
+       <h2>My list of art to see:</h2>
+       <ItemList
+          artworks={myList} 
+          onToggle={handleToggleMyList} />
+       <h2>Your list of art to see:</h2>
+       <ItemList
+          artworks={yourList}
+          onToggle={handleToggleYourList} />
+     </>
+   );
+}
+function ItemList({ artworks, onToggle }) {
+      return ( 
+         <ul> 
+           {artworks.map(artwork => ( 
+              <li key={artwork.id}> 
+                <label> 
+                  <input 
+                    type="checkbox" 
+                    checked={artwork.seen} 
+                    onChange={e => { 
+                      onToggle( 
+                      artwork.id,
+                      e.target.checked 
+                    );
+                 }}
+               />  
+                {artwork.title}
+           </label>
+         </li> 
+         ))} 
+      </ul> 
+   );
+}
